438d6cabcfc1684799c499d2ddac1776b0f6dfdb
Steve Kemp <steve@steve.org.uk>
2022-07-10 06:24:48 +0300

    Show numbers as integers where appropriate.

--- primitive/number.go
+++ primitive/number.go
@@ -1,7 +1,7 @@
 package primitive
 
 import (
-	"strconv"
+	"fmt"
 )
 
 // Number type holds numbers
@@ -9,7 +9,13 @@ type Number float64
 
 // ToString converts this object to a string.
 func (n Number) ToString() string {
-	return strconv.FormatFloat(float64(n), 'g', -1, 64)
+
+	// Is this really an integer?
+	if float64(n) == float64(int(n)) {
+		return fmt.Sprintf("%d", int(n))
+	}
+
+	return fmt.Sprintf("%f", n)
 }
 
 // Type returns the type of this primitive object.
