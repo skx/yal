0f7e05ea843b1b4e87aa24f506152b05efc14184
Steve Kemp <steve@steve.org.uk>
2022-07-10 09:51:37 +0300

    Updated example a little

--- test.lisp
+++ test.lisp
@@ -8,8 +8,8 @@
 
 
 ;; Instead of just (+ 1 2) we allow multiple args
-(print "Our mathematical functions allow 2+ arguments, e.g: %s"
-  (+ 1 2 3 4 5 6))
+(print "Our mathematical functions allow 2+ arguments, e.g: %s = %s"
+  (quote (+ 1 2 3 4 5 6)) (+ 1 2 3 4 5 6))
 
 ;; Define a function, `fact`, to calculate factorials.
 (define fact (lambda (n)
@@ -29,7 +29,7 @@
    (begin
       (print "foo is set to %s" foo)
       (set! foo 3)
-      (print "foo is set to %s" foo)))
+      (print "foo is now set to %s" foo)))
 
 ;;Now we're outside the scope of the `let` so `foo` is nil
 (if foo
@@ -40,7 +40,6 @@
 ;; Define another function, and invoke it
 (define sum2 (lambda (n acc) (if (= n 0) acc (sum2 (- n 1) (+ n acc)))))
 (print "Sum of 1-100: %s" (sum2 100 0))
-
 
 ;; Now create a utility function to square a number
 (define sq (lambda (x) (* x x)))
@@ -55,10 +54,10 @@
 (apply (list -2 -1 0 1 2 3 4 5)
   (lambda (x)
     (begin
-      (if (neg? x) (print "%s is negative" x))
-      (if (zero? x) (print "%s is ZERO" x))
-      (if (even? x) (print "%s is EVEN" x))
-      (if (odd? x)  (print "%s is ODD" x)))))
+      (if (neg? x)  (print "%s is negative" x))
+      (if (zero? x) (print "%s is ZERO"     x))
+      (if (even? x) (print "%s is EVEN"     x))
+      (if (odd? x)  (print "%s is ODD"      x)))))
 
 ;; Test that we can get the correct type of each of our primitives
 (apply (list 1 "steve" (list 1 2 3) #t #f nil boolean? print)
@@ -69,10 +68,10 @@
 ;;
 ;; nth starts counting at zero which is perhaps surprising.
 (if (= (nth (list 10 20 30 40 50) 0) 10)
-    (print "Got the first item of the list"))
+    (print "Got the first item of the list."))
 
 (if (= (nth (list 10 20 30 40 50) 1) 20)
- (print "Got the second item of the list"))
+ (print "Got the second item of the list."))
 
 ;;
 ;; Show even numbers via the filter-function.
@@ -80,22 +79,25 @@
 (print "Even numbers from 0-10: %s"
        (filter (nat 11) (lambda (x) (even? x))))
 
+;;
+;; And again with square numbers.
+;;
 (print "Squared numbers from 0-10: %s"
        (map (nat 11) (lambda (x) (sq x))))
 
 
 ;;
-;; Setup a list and do a few things with it.
+;; Setup a list of integers, and do a few things with it.
 ;;
 (let ((vals '(32 92 109 903 31 3 -93 -31 -17 -3)))
   (begin
      (print "Working with the list: %s " vals)
-     (print "\tBiggest item is %s" (max vals))
-     (print "\tSmallest item is %s" (min vals))
-     (print "\tReversed list is %s "(reverse vals))
-     (print "\tSorted list is %s "(sort vals))
-     (print "\tFirst item is %s "(first vals))
-     (print "\tRemaining items %s "(rest vals))
+     (print "\tBiggest item is %s"       (max vals))
+     (print "\tSmallest item is %s"      (min vals))
+     (print "\tReversed list is %s "     (reverse vals))
+     (print "\tSorted list is %s "       (sort vals))
+     (print "\tFirst item is %s "        (first vals))
+     (print "\tRemaining items %s "      (rest vals))
    ))
 
 ;
