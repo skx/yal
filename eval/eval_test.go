d70a1525e065e897e8aeb31d8c95ea093850ce33
Steve Kemp <steve@steve.org.uk>
2022-07-15 17:14:06 +0300

    Added some trivial tests of the stdlib.
    Since I had inc/dec the wrong way round that seems prudent.

--- eval/eval_test.go
+++ eval/eval_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/skx/yal/stdlib"
 )
 
+// This function contains a bunch of table-driven tests which are
+// designed to be simple.
 func TestEvaluate(t *testing.T) {
 
 	type TC struct {
@@ -112,6 +114,84 @@ func TestEvaluate(t *testing.T) {
 		{"'", "nil"},
 		{"(3 3 ", "nil"},
 		{"(((((", "nil"},
+	}
+
+	for _, test := range tests {
+
+		// Load our standard library
+		st := stdlib.Contents()
+		std := string(st)
+
+		// Create a new interpreter
+		l := New(std + "\n" + test.input)
+
+		// With a new environment
+		env := env.New()
+
+		// Populate the default primitives
+		builtins.PopulateEnvironment(env)
+
+		// Run it
+		out := l.Evaluate(env)
+
+		if out.ToString() != test.output {
+			t.Fatalf("test '%s' should have produced '%s', but got '%s'", test.input, test.output, out.ToString())
+		}
+
+	}
+}
+
+// This function tests our standard library.
+func TestStandardLibrary(t *testing.T) {
+
+	type TC struct {
+		input  string
+		output string
+	}
+
+	tests := []TC{
+
+		// (boolean?
+		{input: "(boolean? true)", output: "#t"},
+		{input: "(boolean? #t)", output: "#t"},
+		{input: "(boolean? false)", output: "#t"},
+		{input: "(boolean? #f)", output: "#t"},
+		{input: "(boolean? \"steve\")", output: "#f"},
+		{input: "(boolean? (1 2 3))", output: "#f"},
+		{input: "(boolean? 3)", output: "#f"},
+
+		// first/last
+		{input: "(first (list 10 11 12))", output: "10"},
+		{input: "(rest  (list 10 11 12))", output: "(11 12)"},
+
+		// inc/dec
+		{input: "(inc 10)", output: "11"},
+		{input: "(dec 11)", output: "10"},
+
+		// zero?/one?
+		{input: "(zero? 0)", output: "#t"},
+		{input: "(zero? 10)", output: "#f"},
+		{input: "(zero? \"steve\")", output: "#f"},
+		{input: "(one? 0)", output: "#f"},
+		{input: "(one? 1)", output: "#t"},
+		{input: "(one? \"steve\")", output: "#f"},
+
+		// map
+		{input: `
+(define sq (lambda (x) (* x x)))
+(map (list 1 2 3 4 5) (lambda (x) (sq x)))
+`,
+			output: "(1 4 9 16 25)"},
+
+		// range
+		{input: "(range -5 5 1)", output: "(-5 -4 -3 -2 -1 0 1 2 3 4)"},
+		{input: "(range 1 11 2)", output: "(1 3 5 7 9)"},
+
+		// seq/nat
+		{input: "(seq 10)", output: "(0 1 2 3 4 5 6 7 8 9)"},
+		{input: "(nat 10)", output: "(1 2 3 4 5 6 7 8 9)"},
+
+		{input: "(join (reverse (split \"Steve\" \"\")))", output: "evetS"},
 	}
 
 	for _, test := range tests {
