c53a497762262b9b7125c5421397edf3dc332c60
Steve Kemp <steve@steve.org.uk>
2022-10-04 21:29:13 +0300

    Ensure we test our size correctly.

--- stdlib/stdlib.go
+++ stdlib/stdlib.go
@@ -15,5 +15,5 @@ var mal string
 // Contents returns the embedded contents of our Lisp standard-library.
 func Contents() []byte {
 
-	return []byte(stdlib + "\n" + mal)
+	return []byte(stdlib + "\n" + mal + "\n")
 }
