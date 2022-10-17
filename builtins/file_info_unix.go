70c5d64d559e1b11d654f4df4b52897410e7ae63
Steve Kemp <steve@steve.org.uk>
2022-10-17 22:25:19 +0300

    Updated the function order & comments.

--- builtins/file_info_unix.go
+++ builtins/file_info_unix.go
@@ -3,31 +3,29 @@
 package builtins
 
 import (
-	"fmt"
 	"os"
 	"syscall"
 )
 
-// getUID returns the owner of the file, from the extended information
-// available after a stat.
-//
-// This is in a seperate file so that we can build upon Windows systems.
-func getUID(info os.FileInfo) (int, error) {
-
-	if stat, ok := info.Sys().(*syscall.Stat_t); ok {
-		return int(stat.Uid), nil
-	}
-	return 0, fmt.Errorf("not found")
-}
-
 // getGID returns the group of the file, from the extended information
-// available after a stat.
+// available after a stat - that is not portable to Windows though.
 //
-// This is in a seperate file so that we can build upon Windows systems.
+// This is in a separate file so that we use build-tags to build code
+// appropriately.
 func getGID(info os.FileInfo) (int, error) {
 
-	if stat, ok := info.Sys().(*syscall.Stat_t); ok {
-		return int(stat.Gid), nil
-	}
-	return 0, fmt.Errorf("not found")
+	stat, _ := info.Sys().(*syscall.Stat_t)
+	return int(stat.Gid), nil
+}
+
+
+// getUID returns the owner of the file, from the extended information
+// available after a stat - that is not portable to Windows though.
+//
+// This is in a separate file so that we use build-tags to build code
+// appropriately.
+func getUID(info os.FileInfo) (int, error) {
+
+	stat, _ := info.Sys().(*syscall.Stat_t)
+	return int(stat.Uid), nil
 }
--- builtins/file_info_windows.go
+++ builtins/file_info_windows.go
@@ -7,20 +7,28 @@ import (
 	"os"
 )
 
-// getUID returns the owner of the file, from the extended information
-// available after a stat.
+// getGID should return the group of the file, from the extended information
+// available after a stat, however on Windows platforms that doesn't work
+// in the obvious way.
 //
-// This is in a seperate file so that we can build upon Windows systems.
-func getUID(info os.FileInfo) (int, error) {
+// Here we just return an error to make that apparent to the caller.
+//
+// This is in a separate file so that we use build-tags to build code
+// appropriately.
+func getGID(info os.FileInfo) (int, error) {
 
 	return 0, fmt.Errorf("not found")
 }
 
-// getGID returns the group of the file, from the extended information
-// available after a stat.
+// getUID should return the owner of the file, from the extended information
+// available after a stat, however on Windows platforms that doesn't work
+// in the obvious way.
 //
-// This is in a seperate file so that we can build upon Windows systems.
-func getGID(info os.FileInfo) (int, error) {
+// Here we just return an error to make that apparent to the caller.
+//
+// This is in a separate file so that we use build-tags to build code
+// appropriately.
+func getUID(info os.FileInfo) (int, error) {
 
 	return 0, fmt.Errorf("not found")
 }
