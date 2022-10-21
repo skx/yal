568e2ebcef5ce66454933d4e4bced7407b76f8a5
Steve Kemp <steve@steve.org.uk>
2022-10-21 21:57:35 +0300

    Updated the stdlib.go, to load *.lisp
    Rather than just loading two files we now load all lisp
    files in this directory.

--- stdlib/stdlib.go
+++ stdlib/stdlib.go
@@ -1,19 +1,50 @@
-// Package stdlib contains a simple/small standard-library, which
-// is written in lisp itself.
+// Package stdlib contains a simple & small standard-library,
+// written in lisp itself.
+//
+// By default our standard library is loaded prior to the
+// execution of any user-supplied code.
 package stdlib
 
 import (
-	_ "embed" // embedded-resource magic
+	"embed" // embedded-resource magic
+	"fmt"
+	"path/filepath"
 )
 
-//go:embed stdlib.lisp
-var stdlib string
-
-//go:embed mal.lisp
-var mal string
+//go:embed *.lisp
+var stdlib embed.FS
 
 // Contents returns the embedded contents of our Lisp standard-library.
+//
+// We embed "*.lisp" when we build our binary.
 func Contents() []byte {
 
-	return []byte(stdlib + "\n" + mal + "\n")
+	// Result
+	result := []byte{}
+
+	// Read the list of entries
+	entries, err := stdlib.ReadDir(".")
+	if err != nil {
+		fmt.Printf("Failed to read embedded resources; fatal error\n")
+		return result
+	}
+
+	// For each entry
+	for _, entry := range entries {
+
+		// Get the filename
+		fp := filepath.Join(".", entry.Name())
+
+		// Read the content
+		data, err := stdlib.ReadFile(fp)
+		if err != nil {
+			fmt.Printf("Failed to read embedded resource - %s - fatal error\n", fp)
+			return result
+		}
+
+		// Append to our result
+		result = append(result, data...)
+	}
+
+	return result
 }
