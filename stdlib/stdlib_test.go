0ddff1d9c5f7746873b4bda38bbd201a5952b04a
Steve Kemp <steve@steve.org.uk>
2022-10-21 22:14:07 +0300

    Minimal test-coverage

--- stdlib/stdlib_test.go
+++ stdlib/stdlib_test.go
@@ -5,27 +5,24 @@ import (
 	"testing"
 )
 
-func TestStdlib(t *testing.T) {
+// Test we can exclude all
+func TestStdlibExcludeAll(t *testing.T) {
+
+	// By default we get "stuff"
 	x := Contents()
 
-	var core []byte
-	var mal []byte
-	var err error
-
-	core, err = os.ReadFile("stdlib.lisp")
-	if err != nil {
-		t.Fatalf("failed to read: %s", err)
+	if len(x) < 1 {
+		t.Fatalf("Failed to get contents of stdlib")
 	}
 
-	mal, err = os.ReadFile("mal.lisp")
-	if err != nil {
-		t.Fatalf("failed to read: %s", err)
+	// Excluding everything should return nothing
+	os.Setenv("YAL_STDLIB_EXCLUDE_ALL", "yes")
+
+	x = Contents()
+	if len(x) != 0 {
+		t.Fatalf("We expected no content, but got something, despite $YAL_STDLIB_EXCLUDE_ALL")
 	}
 
-	// Add one for the newline we add in the middle.
-	total := len(core) + 1 + len(mal) + 1
-	if total != len(x) {
-		t.Fatalf("stdlib size mismatch")
-	}
-
+	// restore
+	os.Setenv("YAL_STDLIB_EXCLUDE_ALL", "")
 }
