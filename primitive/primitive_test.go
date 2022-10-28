c1ef15f26070d8d6f761b96fac315167979607da
Steve Kemp <steve@steve.org.uk>
2022-10-28 05:52:43 +0300

    Test coverage back to 100%

--- primitive/primitive_test.go
+++ primitive/primitive_test.go
@@ -1,6 +1,7 @@
 package primitive
 
 import (
+	"fmt"
 	"strings"
 	"testing"
 )
@@ -19,12 +20,33 @@ func TestBool(t *testing.T) {
 	if false.ToString() != "#f" {
 		t.Fatalf("bool->String had wrong result")
 	}
+
+	ti := true.ToInterface()
+	fi := false.ToInterface()
+
+	bTrue, bOK := ti.(bool)
+	if !bOK {
+		t.Fatalf("bool.ToInterface did not result in a bool")
+	}
+	if !bTrue {
+		t.Fatalf("ToInterface resulted in the wrong result")
+	}
+
+	bFalse, bOK2 := fi.(bool)
+	if !bOK2 {
+		t.Fatalf("bool.ToInterface did not result in a bool")
+	}
+	if bFalse {
+		t.Fatalf("ToInterface resulted in the wrong result")
+	}
+
 }
 
 func TestCharacter(t *testing.T) {
 
 	nl := Character("\n")
 	ok := Character("o")
+	empty := Character("")
 
 	if nl.Type() != "character" {
 		t.Fatalf("wrong type")
@@ -34,6 +56,25 @@ func TestCharacter(t *testing.T) {
 	}
 	if ok.ToString() != "o" {
 		t.Fatalf("char->String had wrong result")
+	}
+
+	nli := nl.ToInterface()
+	emptyi := empty.ToInterface()
+
+	nliGo, nliOK := nli.(uint8)
+	if !nliOK {
+		t.Fatalf("character.ToInterface gave wrong type %T", nli)
+	}
+	if nliGo != '\n' {
+		t.Fatalf("ToInterface resulted in the wrong result")
+	}
+
+	emptyGo, emptyOK := emptyi.(string)
+	if !emptyOK {
+		t.Fatalf("character.ToInterface gave wrong type %T", emptyi)
+	}
+	if emptyGo != "" {
+		t.Fatalf("ToInterface resulted in the wrong result")
 	}
 }
 
@@ -52,6 +93,14 @@ func TestError(t *testing.T) {
 		t.Fatalf("arity-error is non-obvious")
 	}
 
+	//
+	// TODO: This is horrid
+	//
+	errGo := error.ToInterface()
+	if !strings.Contains(fmt.Sprintf("%s", errGo), "cheese") {
+		t.Fatalf("error.ToInterface is non-obvious")
+	}
+
 }
 
 func TestIsNil(t *testing.T) {
@@ -63,6 +112,11 @@ func TestIsNil(t *testing.T) {
 	}
 	if n.ToString() != "nil" {
 		t.Fatalf("nil->string wrong result")
+	}
+
+	i := n.ToInterface()
+	if i != nil {
+		t.Fatalf("nil.ToInterface gave wrong type")
 	}
 
 	var s String
@@ -112,6 +166,26 @@ func TestNumber(t *testing.T) {
 	if !(strings.Contains(f.ToString(), "0.111")) {
 		t.Fatalf("number->String (float) had wrong result:%s", f.ToString())
 	}
+
+	ii := i.ToInterface()
+	fi := f.ToInterface()
+
+	iGo, iOK := ii.(int)
+	if !iOK {
+		t.Fatalf("Int.ToInterface gave wrong type %T", ii)
+	}
+	if iGo != 3 {
+		t.Fatalf("ToInterface resulted in the wrong result")
+	}
+
+	fGo, fOK := fi.(float64)
+	if !fOK {
+		t.Fatalf("Int.ToInterface gave wrong type")
+	}
+	if !(strings.Contains(fmt.Sprintf("%f", fGo), "0.111")) {
+		t.Fatalf("ToInterface resulted in the wrong result")
+	}
+
 }
 
 func TestString(t *testing.T) {
@@ -136,4 +210,15 @@ func TestSymbol(t *testing.T) {
 	if sym.ToString() != "pi" {
 		t.Fatalf("symbol->String had wrong result")
 	}
+
+	si := sym.ToInterface()
+
+	sGo, sOK := si.(string)
+	if !sOK {
+		t.Fatalf("String.ToInterface gave wrong type")
+	}
+	if sGo != "pi" {
+		t.Fatalf("ToInterface resulted in the wrong result")
+	}
+
 }
