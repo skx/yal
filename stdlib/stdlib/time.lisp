c50a00f8181c75e6fc83819b96ea6ebdc5279acf
Steve Kemp <steve@steve.org.uk>
2022-11-10 08:07:37 +0200

    Simplify the implementation of hms, by dropping the zero-padding and just using a format string.  D'oh

--- stdlib/stdlib/time.lisp
+++ stdlib/stdlib/time.lisp
@@ -24,19 +24,11 @@
        minute time:minute
        second time:second)
 
-(set! zero-pad-single-number (fn* (num:number)
-                                  "Return the given number, padded to two digits, as a string.
-i.e. Add a '0' prefix to the specified number, for values less than ten.
-
-This is designed to pad the hours, minutes, and seconds in (hms)."
-                                  (if (< num 10)
-                                      (sprintf "0%d" num)
-                                    (str num))))
 
 (set! time:hms (fn* ()
                "Return the current time as a string, formatted as 'HH:MM:SS'."
-               (sprintf "%s:%s:%s"
-                        (zero-pad-single-number (hour))
-                        (zero-pad-single-number (minute))
-                        (zero-pad-single-number (second)))))
+               (sprintf "%02d:%02d:%02d"
+                        (hour)
+                        (minute)
+                        (second))))
 (alias hms time:hms)
