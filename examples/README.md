25191939d63820a80b88d3c6435731410ed2531f
Steve Kemp <steve@steve.org.uk>
2022-11-21 20:55:22 +0200

    Added helper-script to update README.md
    This uses yal to generate a README.md file, using itself :D

---
+++ examples/README.md
@@ -0,0 +1,27 @@
+# Examples
+
+This directory contains some simple lisp examples, which can be executed via `yal`.
+
+
+* [dynamic.lisp](dynamic.lisp)
+  * Execute code by name, via introspection.
+* [fibonacci.lisp](fibonacci.lisp)
+  * Calculate the first 25 fibonacci numbers.
+* [fizzbuzz.lisp](fizzbuzz.lisp)
+  * A simple fizzbuzz implementation
+* [hash.lisp](hash.lisp)
+  * Demonstrate working with hashes.
+* [lisp-tests.lisp](lisp-tests.lisp)
+  * Some simple tests of our lisp-written primitives
+* [mtest.lisp](mtest.lisp)
+  * Simple tests of our macro system.
+* [readme.lisp](readme.lisp)
+  * Generate a README.md file, based on directory contents.
+* [sorting.lisp](sorting.lisp)
+  * Demonstrate generating random lists, and sorting them.
+* [test.lisp](test.lisp)
+  * Simple feature-tests/demonstrations of our system.
+* [time.lisp](time.lisp)
+  * Demonstrate the date/time functions
+* [try.lisp](try.lisp)
+  * Demonstrate our try/catch behaviour
---
+++ examples/readme.lisp
@@ -0,0 +1,37 @@
+;;; readme.lisp - Generate a README.md file, based on directory contents.
+
+;;
+;; This directory contains *.lisp, echo of which has a header-line prefixed
+;; with three semi-colons:
+;;
+;;     ;;; foo.lisp - Information
+;;
+;; This script reads those files and outputs a simple index, of the filename
+;; and the information.
+;;
+
+(set! lisp:files (fn* ()
+                      "Return a list of all the lisp files in the current directory"
+                      (sort (glob "*.lisp"))))
+
+
+(set! lisp:info (fn* (file)
+                     "Output a brief overview of the given file"
+                     (let* (
+                            text (file:lines file)
+                            line  (nth text 0)
+                            info (match "^([^-]*)-+[ ]+(.*)$" line))
+                       (when (list? info)
+                         (print "* [%s](%s)" file file)
+                         (print "  * %s" (nth info 2))))))
+
+
+(set! lisp:index (fn* ()
+                      "Generate a README.md snippet"
+                      (let* (files (lisp:files))
+                        (apply files lisp:info()))))
+
+(print "# Examples\n")
+(print "This directory contains some simple lisp examples, which can be executed via `yal`.\n\n")
+
+(lisp:index)
