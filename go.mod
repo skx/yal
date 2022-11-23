35228f243933fe2153eb5ddacdfed350679674d0
Steve Kemp <steve@steve.org.uk>
2022-11-23 05:25:38 +0200

    Added initial LSP support.
    This pull-request, once complete, will had LSP support and
    will close #108.
    
    Currently we show help for standard library functions, when
    the user hovers over a token.  We also provide completion for
    all our standard-library functions.

--- go.mod
+++ go.mod
@@ -1,3 +1,28 @@
 module github.com/skx/yal
 
 go 1.18
+
+require (
+	github.com/tliron/glsp v0.1.1
+	github.com/tliron/kutil v0.1.63
+	go.lsp.dev/uri v0.3.0
+)
+
+require (
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/iancoleman/strcase v0.2.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/muesli/termenv v0.12.0 // indirect
+	github.com/petermattis/goid v0.0.0-20180202154549-b0b1615b78e5 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/sasha-s/go-deadlock v0.3.1 // indirect
+	github.com/sourcegraph/jsonrpc2 v0.1.0 // indirect
+	github.com/vmihailenco/msgpack/v5 v5.3.5 // indirect
+	github.com/vmihailenco/tagparser/v2 v2.0.0 // indirect
+	golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa // indirect
+	golang.org/x/sys v0.0.0-20220728004956-3c1f35247d10 // indirect
+	golang.org/x/term v0.0.0-20220722155259-a9ba230a4035 // indirect
+)
---
+++ lsp.go
@@ -0,0 +1,178 @@
+// This file provides our LSP support.
+//
+// When yal is invoked with the "-lsp" flag we call lspStart(),
+// which provides simple completion and hover support.
+//
+
+package main
+
+import (
+	"fmt"
+	"os"
+	"sort"
+
+	"github.com/skx/yal/primitive"
+	"github.com/tliron/glsp"
+	protocol "github.com/tliron/glsp/protocol_3_16"
+	"github.com/tliron/glsp/server"
+	"github.com/tliron/kutil/logging"
+	"go.lsp.dev/uri"
+
+	// Must include a backend implementation. See kutil's logging/ for other options.
+	_ "github.com/tliron/kutil/logging/simple"
+)
+
+// lsName contains the name of our LSP handler
+const lsName = "yal"
+
+// handler contains the pointer to our handler
+var handler protocol.Handler
+
+// initialize is called to setup a new buffer.
+func initialize(context *glsp.Context, params *protocol.InitializeParams) (any, error) {
+	capabilities := handler.CreateServerCapabilities()
+
+	return protocol.InitializeResult{
+		Capabilities: capabilities,
+		ServerInfo: &protocol.InitializeResultServerInfo{
+			Name:    "yal",
+			Version: &version,
+		},
+	}, nil
+}
+
+func lspStart() {
+	logging.Configure(1, nil)
+
+	handler = protocol.Handler{
+		// generic
+		Initialize: initialize,
+
+		// Yal Specific
+		TextDocumentCompletion: textDocumentCompletion,
+		TextDocumentHover:      textDocumentHover,
+	}
+
+	server := server.NewServer(&handler, lsName, false)
+
+	server.RunStdio()
+}
+
+// textDocumentCompletion should return appropriate completions.
+//
+// However we just always return a list of all known functions, the
+// client can sort it out.
+func textDocumentCompletion(context *glsp.Context, params *protocol.CompletionParams) (interface{}, error) {
+
+	// Build up a list of all things known in the environment
+	keys := []string{}
+
+	// Save the known "things", because we want show them in sorted-order.
+	items := ENV.Items()
+	for k := range items {
+		keys = append(keys, k)
+	}
+
+	// sort the known-things (i.e. environment keys)
+	sort.Strings(keys)
+
+	// Create the return value
+	out := make([]protocol.CompletionItem, len(keys))
+
+	// The kind of completion we have
+	kind := protocol.CompletionItemKindFunction
+
+	// Now we have a list of sorted things.
+	for i, key := range keys {
+
+		out[i] = protocol.CompletionItem{
+			Label:  key,
+			Kind:   &kind,
+			Detail: &key,
+		}
+	}
+	return out, nil
+}
+
+// textDocumentHover is called when the client hovers over a token
+//
+// We need to find out what text is being hovered over, and return
+// something "useful" to the client.
+func textDocumentHover(context *glsp.Context, params *protocol.HoverParams) (*protocol.Hover, error) {
+
+	// Get the file
+	_uri, err := uri.Parse(params.TextDocument.URI)
+	if err != nil {
+		return nil, err
+	}
+
+	// open the file
+	var content []byte
+
+	// read the content
+	content, err = os.ReadFile(_uri.Filename())
+	if err != nil {
+		return nil, err
+	}
+
+	// We'll build up the current line, being hovered on here
+	var curLine uint32
+	line := ""
+
+	// count the newlines to get the current line.
+	for _, chr := range content {
+		if chr == '\n' {
+			curLine++
+			continue
+		}
+		if curLine == params.Position.Line {
+			line += string(chr)
+		}
+	}
+
+	// current line is empty?  Then abort
+	if line == "" {
+		return nil, nil
+	}
+
+	// Right now we have the line we want the token
+	//
+	// Assume we have a line like "(this is (cake))"
+	// and position points to the "c" we want to have the
+	// whole token
+	//
+	token := ""
+
+	for i, chr := range line {
+		if chr == rune(' ') || chr == rune('(') || chr == rune(')') || chr == rune('\t') {
+			if uint32(i) > params.Position.Character {
+				break
+			}
+			token = ""
+			continue
+		}
+		token += string(chr)
+	}
+
+	// Find the details of the function, if we can
+	info, ok := ENV.Get(token)
+	if !ok {
+		return nil, nil
+	}
+
+	// Is it a procedure?
+	prc, ok2 := info.(*primitive.Procedure)
+	if !ok2 {
+		return nil, nil
+	}
+
+	// The text we'll show.
+	help := fmt.Sprintf("**%s**\n%s", token, prc.Help)
+
+	return &protocol.Hover{
+		Contents: protocol.MarkupContent{
+			Kind:  protocol.MarkupKindMarkdown,
+			Value: help,
+		},
+	}, nil
+}
